4331f5ee4a9c516483eb7d5736279c74a8cfaa15
tezaharsony <teza.harsony2303942gmail.com>
2017-07-13 22:04:17 +0700

    website can add student and view data from student, teacher and subject

--- databases/Routers/student.js
+++ databases/Routers/student.js
@@ -10,4 +10,43 @@ router.get("/", function(req,res){
   })
 })
 
+router.get('/add', function(req, res){
+  res.render('studentAdd');
+});
+
+//add new student req.body
+router.post('/add', function(req, res){
+  model.Student.create({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email})
+  .then( function(){
+    res.redirect('/student');
+  })
+});
+
+//go to the student edit form
+router.get('/edit/:id', function(req, res){
+  model.Student.findById(req.params.id)
+  .then (function (rows){
+    res.render('studentEdit', {data_student: rows});
+  })
+   });
+
+router.post('/edit/:id', function(req, res) {
+  model.Student.update({ first_name: req.body.first_name, last_name: req.body.last_name, email: req.body.email},
+    {
+      where: {id: req.params.id}
+    }
+  )
+  .then( function(){
+  res.redirect('/student');
+  })
+});
+
+//delete data from student
+router.get('/delete/:id', function(req, res){
+    model.Student.destroy({where: {id : req.params.id}})
+    .then( function(){
+  res.redirect('/student');
+  })
+});
+
 module.exports = router;
--- databases/Routers/teacher.js
+++ databases/Routers/teacher.js
@@ -12,5 +12,4 @@ router.get("/", function(req,res){
 })
 
 
-
 module.exports = router;
